ddd58c4162b6e9a9c66f5b0476f52c29df0e7e23
XynO1234 <suthinna2007@gmail.com>
2024-03-18 22:31:02 +0700

    xgfdhths

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ $(document).ready(function(){
     var fruit = 0;
     var treeLevel = 1;
     var oxygenNeeded = 10;
-    var pickaxes = 0;
-    var money = 0;
     var logPlus = 1;
     var autoLogPlus = 0;
-    var autoChopperPrice = 100;
-    var pickaxePrice = 50;
-    var logPrice = 1;
+    var autoClickerPrice = 100;
     var menu;
 
     setInterval(function(){
@@ -18,7 +14,7 @@ $(document).ready(function(){
         changeMarket();
     }, 1000);
 
-    $("#chop").click(function(){
+    $("#click").click(function(){
         oxygen += logPlus;
         if ((oxygen % 10)== 0) {
             fruit += logPlus;
@@ -41,30 +37,13 @@ $(document).ready(function(){
         changeMarket();
     });
 
-    $("#sell10").click(function(){
-        oxygen-=10;
-        money += logPrice * 10;
-        changeInventory();
-        changeMarket();
-    });
-
-    $("#sellAll").click(function(){
-        money += logPrice * oxygen;
-        oxygen = 0;
-        changeInventory();
-        changeMarket();
-    });
-
-    $("#autoChopper").click(function(){
-        money -= autoChopperPrice;
-        autoLogPlus++;
-        changeInventory();
-        changeMarket();
-    });
-
-    $("#buyPickaxe").click(function(){
-        money -= pickaxePrice;
-        pickaxes++;
+    $("#autoClicker").click(function(){
+        if (oxygen < autoClickerPrice) {
+            alert("You don't have enough Oxygen!")
+        } else {
+            oxygen -= autoClickerPrice;
+            autoLogPlus++;
+        }
         changeInventory();
         changeMarket();
     });
@@ -81,7 +60,7 @@ $(document).ready(function(){
 
 
     function changeInventory(){
-        $("#money").html("Money: $" + money);
+        
 
         if(oxygen == 1){
             $("#oxygen").html("Oxygen Level: " + oxygen);
@@ -89,11 +68,6 @@ $(document).ready(function(){
             $("#oxygen").html("Oxygen Level: " + oxygen);
         }
 
-        if(fruit == 1){
-            $("#fruit").html("Fruits Collected: " + fruit);
-        }else{
-            $("#fruit").html("Fruits Collected: " + fruit);
-        }
 
         if(treeLevel == 1){
             $("#treelevel").html("Tree Level: " + treeLevel);
@@ -101,41 +75,6 @@ $(document).ready(function(){
             $("#treelevel").html("Tree Level: " + treeLevel);
         }
 
-        if(pickaxes > 0){
-            $("#pickaxes").html("You now own " + pickaxes + " pickaxe(s).");
-        }else{
-            $("#pickaxes").html("");
-        }
-    }
-
-    function changeMarket(){
-        if(oxygen > 0){
-            $("#sellAll").css("display", "block");
-        }else{
-            $("#sellAll").css("display", "none");
-        }
-        if(oxygen >= 1){
-            $("#sell1").css("display", "block");
-        }else{
-            $("#sell1").css("display", "none");
-        }
-        if(oxygen >= 10){
-            $("#sell10").css("display", "block");
-        }else{
-            $("#sell10").css("display", "none");
-        }
-
-        if(money >= autoChopperPrice){
-            $("#autoChopper").css("display", "block");
-        }else{
-            $("#autoChopper").css("display", "none");
-        }
-
-        if(money >= pickaxePrice){
-            $("#buyPickaxe").css("display", "block");
-        }else{
-            $("#buyPickaxe").css("display", "none");
-        }
     }
 
     function switchMenu(menu){
